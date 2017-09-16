673fc9caf276e317fbca2c05f947212d73ad9876
Ryan Holstein <Ryan.c.Holstein@biola.edu>
2017-09-16 16:00:38 -0700

    index.js added

--- static/styles/index.js
+++ static/styles/index.js
@@ -1,17 +1,17 @@
-//
-// $(document).ready(function(){
-//
-//   if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-//
-//     $("#center").css({
-//       "width": "300px",
-//       "margin-left": "-150px"
-//     });
-//
-//     $(".main-title").css({
-//       "font-size": "36px"
-//     });
-//
-//   }
-//
-// });
+
+$(document).ready(function(){
+
+  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+
+    $("#center").css({
+      "width": "300px",
+      "margin-left": "-150px"
+    });
+
+    $(".main-title").css({
+      "font-size": "36px"
+    });
+
+  }
+
+});
